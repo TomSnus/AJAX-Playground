ca0718a351aca8b853bd26474cd9bf77a90c09c6
TomSnus <senteswins@gmail.com>
2018-08-28 18:23:57 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -12,8 +12,31 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
+    var gMapsUrl ='http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+    $body.append('<img class="bgimg" src="' + gMapsUrl + address +'">');
     // YOUR CODE GOES HERE!
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': "e34e96f934f3468b8c0b13c129256892",
+        'q': address
+    });
+
+    $.getJSON(url, function( data ) {
+
+        $nytHeaderElem.text('New York Times Articles about ' + cityStr);
+        var articles =  data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+            '<a href="'+article.web_url+'">'+article.headline.main+'</a>'
+            +'<p>'+article.snippet+ '</p>'
+            +'</li>');
+        }
+    });
 
     return false;
 };
